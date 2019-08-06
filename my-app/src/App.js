55f4d17068876dc186723da15d67fe56fdf835b9
Emmanuel Tanifum <etantanifum@gmail.com>
2019-08-06 18:37:04 -0400

    data got from api

---
+++ my-app/src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './App.css';
+import Axios from 'axios';
+import UserCard from './components/UserCard';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = []
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData = () => {
+    Axios('https://api.github.com/users/Manny-Rocks')
+      .then(res => {
+        this.setState(res.data)
+        console.log('res.data ', res.data)
+        console.log('this.state ', this.state)
+      })
+      .catch(err => {
+        console.log('err ', err)
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>GitHub Usercard</h1>
+        <UserCard 
+          props={this.state}
+        />
+      </div>
+    );
+  
+  }
+}
+
+export default App;
